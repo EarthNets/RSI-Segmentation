337285d802f04e35ff11ebff07b51266bb4e7375
ShadowXZT <xiongzhitong@gmail.com>
2022-05-19 09:12:16 +0000

    modify dataset

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "Dataset4EO",
+    name = "RSI-Segmentation",
     version = "0.0.1",
     author = "Zhitong Xiong",
     author_email = "xiongzhitong@gmail.com",
     description = ("Dataset loaders for Remote Sensing datasts on Earth observation tasks."),
     license = "BSD",
-    keywords = "Remote sensing, Datasets, Geospatial",
-    url = "http://packages.python.org/Dataset4EO",
-    packages=['Dataset4EO', 'tests'],
+    keywords = "Remote sensing image segmentation",
+    url = "http://packages.python.org/RSI-Segmentation",
+    packages=['rsiseg', 'tests'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
